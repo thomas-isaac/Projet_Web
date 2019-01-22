e167d0d8392d0bb4d44f52a15bc36f831f45138c
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-22 10:55:21 +0100

    Add files via upload

---
+++ Toolbox/footer.php
@@ -0,0 +1,10 @@
+<footer>
+       <div id="footerlogo">
+        <a href="mentions.php#mentions" id="mentions">Mentions légales</a>
+        <a href="https://fr-fr.facebook.com/bdecesirouen/"><img src="pictures/facebook.png" id="facebook"></a>
+        <a href="https://twitter.com/bdecesirouen?lang=fr"><img src="pictures/twitter.png" id="twitter"></a>
+        <a href="https://www.youtube.com/channel/UCO70xdTWU7T_cxAtvRbCGmw"><img src="pictures/youtube.png" id="youtube"></a>
+        <a href="mailto:bde-rouen@viacesi.fr"><img src="pictures/gmail.png" id="gamail"></a>
+        <a href="mentions.php#politique" id="mentions">Politique de confidentialité</a>
+       </div>
+</footer>
---
+++ Toolbox/header.php
@@ -0,0 +1,14 @@
+<header>
+         <div id="conteneur">
+          <img src="pictures/Logo_CESI.png" id="Logo_CESI" class="element">
+          <img src="pictures/Logo_BDE.png" id="Logo_BDE" class="element">
+          <a id="btn1" href="connexion.php">Connexion</a>
+        </div>
+
+        <div id="conteneur1">
+          <a id="btn" href="accueil.blade.php">Accueil</a>
+          <a id="btn" href="boutique.php">Boutique</a>
+          <a id="btn" href="event.php">Events</a>
+          <a id="btn" href="boite.php">Boite à idées</a>
+        </div>
+</header>
