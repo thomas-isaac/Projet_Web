4632df3d99b8ea5f4bae38a9ec69ca6fba05f123
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-23 10:23:19 +0100

    Create accueil.php

---
+++ Toolbox/accueil.php
@@ -0,0 +1,42 @@
+<!doctype html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">        <link rel="stylesheet"href="/style/vendors/fontawesome/css/all.min.css"/>
+        <link href= "style.css" rel="stylesheet"  type="text/css"/>
+        <link rel="icon" type="image/png" href="pictures/Logo_BDE.png"/>      
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Les PANGU du CESI</title>
+    </head>
+
+<body>
+        <?php include("header.php"); ?>
+
+        <main>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+          <p>accueil</P>
+        </main>
+
+     </body>
+
+     <?php include("footer.php"); ?>
+
+</html>
