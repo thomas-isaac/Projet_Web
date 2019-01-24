73911e0c9084aa49e22ea546aa3fb141678e9ec5
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-24 15:12:23 +0100

    Add files via upload

---
+++ Toolbox/headerBoutique.php
@@ -0,0 +1,18 @@
+
+<header>
+         <div id="conteneur">
+          <img src="pictures/Logo_CESI.png" id="Logo_CESI" class="element">
+          <img src="pictures/Logo_BDE.png" id="Logo_BDE" class="element">
+          <div id="conteneur15">
+            <a id="btn1" href="connexion.php" style="width: 75%; height: 9%; font-size: 150%;">Connexion</a>
+            <a href="panier.php" id="panier"><img src="pictures/panier.png" id="panier"></a>
+          </div>
+        </div>
+
+        <div id="conteneur1">
+          <a id="btn" href="accueil.php">Accueil</a>
+          <a id="btn" href="boutique.php">Boutique</a>
+          <a id="btn" href="event.php">Events</a>
+          <a id="btn" href="boite.php">Boite à idées</a>
+        </div>
+</header>
