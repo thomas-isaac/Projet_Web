0b4854149f9bc0ceed3cb698c53c85df253013a2
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-24 15:04:28 +0100

    Update connexion.php

--- Toolbox/connexion.php
+++ Toolbox/connexion.php
@@ -12,53 +12,40 @@
 </head>
 
 <body>
-    <header>
-     <div id="conteneur">
-      <img src="pictures/Logo_CESI.png" id="Logo_CESI" class="element">
-      <img src="pictures/Logo_BDE.png" id="Logo_BDE" class="element">
-      <a id="btn1" href="connexion.php">Connexion</a>
-    </div>
-
-    <div id="conteneur1">
-      <a id="btn" href="accueil.blade.php">Accueil</a>
-      <a id="btn" href="boutique.php">Boutique</a>
-      <a id="btn" href="event.php">Events</a>
-      <a id="btn" href="boite.php">Boite à idées</a>
-    </div>
-    </header>
-
+    
+    <?php include("header.php"); ?>
+    
     <main>
 
-    <form id="form" method="post" action="scriptConnexion.php" autocomplete="on">
-                                <h1>Connexion</h1> 
-                                <p> 
-                                    <label for="mail" class="uname" data-icon="u" > Mail : </label>
-                                    <input id="mail" name="mail" required="required" type="text" placeholder="mail"/>
-                                </p>
-                                <p> 
-                                    <label for="motDePasse" class="youpasswd" data-icon="p"> Mot de passe : </label>
-                                    <input id="password" name="motDePasse" required="required" type="password" placeholder="Mot de passe" /> 
-                                </p>
-                            
-                                <p class="login button"> 
-                                    <input type="submit" value="Connexion" /> 
-                                </p>
-                                <p class="change_link">
-                                    Pas encore inscrit ?
-                                    <a href="inscription.php" class="to_register">Inscription</a>
-                                </p>
-                                <p class="change_link">
-                                    déconnection ?
-                                    <a href="logout.php" class="to_register">logout</a>
-                                </p>
-                            </form>
+   <form id="form" method="post" action="scriptConnexion.php" autocomplete="on">
+            <h1>Connexion</h1><br>
+            <p> 
+              <label for="mail" class="uname" data-icon="u" > Mail : </label>
+              <br>
+              <input id="mail" name="mail" required="required" type="text" placeholder="Mail"/>
+            </p>
+            <br>
+            <p> 
+              <label for="motDePasse" class="youpasswd" data-icon="p"> Mot de passe : </label>
+              <br>
+              <input id="password" name="motDePasse" required="required" type="password" placeholder="Mot de passe" /> 
+            </p><br>
+            <p class="login button" > 
+              <input type="submit" value="Connexion" id="login-button" /> 
+            </p><br>
+            <p class="change_link">Pas encore inscrit ?<br><a href="inscription.php" class="to_register">Inscription</a></p><br>
+            <p class="change_link">
+               Voulez-vous vous déconnecter ?<br>
+               <a href="logout.php" class="to_register">logout</a>
+            </p>
+    </form>
+    <br>
+    
     </main>
 
  </body>
 
- <footer>
-   <h5>coucou</h5>
- </footer>
+ <?php include("footer.php"); ?>
 
 </html>
 
