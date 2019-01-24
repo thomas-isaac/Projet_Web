1539422b3cbecf79b845e397b1d475436b8e0b6e
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-24 15:11:38 +0100

    Update header.php

--- Toolbox/header.php
+++ Toolbox/header.php
@@ -2,7 +2,7 @@
          <div id="conteneur">
           <img src="pictures/Logo_CESI.png" id="Logo_CESI" class="element">
           <img src="pictures/Logo_BDE.png" id="Logo_BDE" class="element">
-          <a id="btn1" href="connexion.php">Connexion</a>
+          <a id="btn1" href="connexion.php" style="width: 9%; height: 8%; font-size: 150%;">Connexion</a>
         </div>
 
         <div id="conteneur1">
