20bfd5359e633256aee9a3755c2bec9f31838f0b
ClemBlue <clement.blin1@viacesi.fr>
2019-01-25 15:24:29 +0100

    Add files via upload

---
+++ Toolbox/dl.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">        
+        <link rel="stylesheet"href="/style/vendors/fontawesome/css/all.min.css"/>
+        <link href= "style.css" rel="stylesheet"  type="text/css"/>
+        <link rel="icon" type="image/png" href="pictures/Logo_BDE.png"/>      
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Page de téléchargement</title>
+    </head>
+
+<body>
+        <?php include("header.php"); ?>
+
+        <main>
+          <div id="adminpage">
+          <div id="download">
+          <h2>Télécharger toutes les photos</h2><br />
+          <p><a id="btn2" href="pictures.zip">Télécharger</a></p>  
+          </div>
+          <div id="signaler">
+          <h2>Choisir la photo que vous voulez télécharger</h2><br/>
+          <p><a id="btn2" href="pictures/">Télécharger</a></p>
+          </div>
+          </div>
+        </main>
+
+     </body>
+
+     <?php include("footer.php"); ?>
+
+</html>
