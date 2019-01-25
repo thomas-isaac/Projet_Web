52acfc5744d9eb41ddbcb6cdc06721f0dda270d1
ClemBlue <clement.blin1@viacesi.fr>
2019-01-25 15:11:32 +0100

    Maj de la boite à idée

--- Toolbox/Aevent.php
+++ Toolbox/Aevent.php
@@ -4,7 +4,8 @@
 
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">        <link rel="stylesheet"href="/style/vendors/fontawesome/css/all.min.css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">        
+        <link rel="stylesheet"href="/style/vendors/fontawesome/css/all.min.css"/>
         <link href= "style.css" rel="stylesheet"  type="text/css"/>
         <link rel="icon" type="image/png" href="pictures/Logo_BDE.png"/>      
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,82 +14,48 @@
     </head>
 
 <body>
-        <header>
-         <div id="conteneur">
-          <img src="pictures/Logo_CESI.png" id="Logo_CESI" class="element">
-          <img src="pictures/Logo_BDE.png" id="Logo_BDE" class="element">
-          <a id="btn1" href="connexion.php">Connexion</a>
-        </div>
-
-        <div id="conteneur1">
-          <a id="btn" href="accueil.blade.php">Accueil</a>
-          <a id="btn" href="boutique.php">Boutique</a>
-          <a id="btn" href="event.php">Events</a>
-          <a id="btn" href="boite.php">Boite à idées</a>
-        </div>
-        </header>
-
+        <?php include("header.php"); ?>
         <main>
-
-        <?php
-        session_start();
-
+          <div id="wrap">
+                  <?php
                 try
-
                 {
-
                     $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
-
                 }
-
                 catch(Exception $e)
-
                 {
-
                         die('Erreur : '.$e->getMessage());
-
                 }
-
                 $reponse = $bdd->query('
                 SELECT * FROM `event` 
                 INNER JOIN avoir ON event.ID_EVENT = avoir.ID_EVENT
-                INNER JOIN image ON avoir.ID_IMAGE = image.ID_IMAGE
-                WHERE event.VALIDATION = 0  AND event.MOIS_EVENT = 0 ');
-
-
+                INNER JOIN image ON avoir.ID_IMAGE = image.ID_IMAGE 
+                WHERE event.VALIDATION = 0 AND event.MOIS_EVENT =0 ');
 
                 while ($donnees = $reponse->fetch())
-
                 {
-
                 ?>
-
+                    <div id="event">
                     <p id="kiloDisplay">
 
-                    <strong>Nom :</strong> <?php echo $donnees['NOM_EVENT']; ?><br />
+                    <a id="btn2" href="admin.php"> <strong>Nom :</strong> <?php echo $donnees['NOM_EVENT']; ?><br /></a>
 
-                    Image : <?php       
-            echo '<img src="public/image/'.$donnees['LIEN_IMAGE'].'"/>';
-                           ?>  <br />       
+                    <p><strong>Image :</strong></p> <?php       
+            echo '<img id="imageproduit" src="public/image/'.$donnees['LIEN_IMAGE'].'"/>';
+                           ?>  <br />
 
 
-
-                </p>
+                </p></div>
 
                 <?php
-
                   }
-
                  $reponse->closeCursor(); 
-
                 ?>
-
+</div>
         </main>
 
      </body>
 
-     <footer>
-       <h5>coucou</h5>
-     </footer>
+    <?php include("footer.php"); ?>
 
 </html>
