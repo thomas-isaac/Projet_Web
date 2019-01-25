9b9a135119ad7daae31fe3163d523552e9dc79bd
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-25 15:38:04 +0100

    Update event.php

--- Toolbox/event.php
+++ Toolbox/event.php
@@ -20,7 +20,6 @@
         <main>
 
         <?php
-        session_start();
                 try
                 {
                     $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
