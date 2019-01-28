6bd313fe99c1548a1068402818d903d36459967e
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-28 08:50:30 +0100

    Update boutique.php

--- Toolbox/boutique.php
+++ Toolbox/boutique.php
@@ -17,6 +17,40 @@
 
 
         <main>
+            <div id="slider" class="carousel slide" data-ride="carousel">
+  				<ol class="carousel-indicators">
+    				<li data-target="#slider" data-slide-to="0" class="active"></li>
+    				<li data-target="#slider" data-slide-to="1"></li>
+    				<li data-target="#slider" data-slide-to="2"></li>
+  				</ol>
+  				<div class="carousel-inner">
+    			<div class="carousel-item active">
+                      <img class="d-block w-100" src="pictures/first.png" alt="First slide">
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Gala du BDE 2018</h3>
+                      </div>
+    			</div>
+   				<div class="carousel-item">
+                      <img class="d-block w-100" src="pictures/second.png" alt="Second slide">
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Acheter les bonnets du BDE ! Ils sont beaux, ils sont frais !</h3>
+                        </div>
+    			</div>
+    			<div class="carousel-item">
+                      <img class="d-block w-100" src="pictures/third.png" alt="Third slide">
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Vous avez une idée d'événement et vous voulez la partager ? Allez dans la boite à idées !</h3>
+                      </div>
+    			</div>
+  				<a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+    				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Previous</span>
+  				</a>
+  				<a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+    				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+   					<span class="sr-only">Next</span>
+  				</a>
+			  </div>
           <div id="wrap">
         <?php
 
@@ -74,5 +108,7 @@
      </body>
 
     <?php include("footer.php"); ?>
-
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </html>
