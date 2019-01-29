c61c8225d73bf4487df9c972d416fa0c48529eb5
pierre-Fleury <33660237+pierre-Fleury@users.noreply.github.com>
2019-01-29 11:16:54 +0100

    Update logout.php

--- Toolbox/logout.php
+++ Toolbox/logout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-unset($_SESSION['log']);
-unset($_SESSION['logBDE']);
-unset($_SESSION['logcesi']);
+$_SESSION['log'] = 0;
+$_SESSION['logBDE'] = 0;
+$_SESSION['logcesi'] = 0;
 
 header('location: connexion.php');
 exit;
